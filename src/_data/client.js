c7b0ba80b70d47675646a166a155c1a97715be56
andrewbramwell <andy@voucherstore.io>
2025-08-30 16:40:05 +0100

    first commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,24 +1,194 @@
 module.exports = {
-    "name": "Splendid Sites Web Designs",
-    "email": "help@splendidsites.com",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "openingHours": "Mon-Sun: 9am-9pm",
-    "onlineBooking": "https://www.facebook.com/",
+    // Main business information
+    "name": "Nutcha Thai Massage Group",
+    "email": "nutchathaimassagegroup@gmail.com",
+    "phoneForTel": "07387639263",
+    "phoneFormatted": "07387 639263",
+    "openingHours": "Mon-Sun: 10am-8pm",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "lineOne": "440 Chester Rd N",
+        "lineTwo": "",
+        "city": "The Royal Town of Sutton Coldfield",
+        "state": "Birmingham",
+        "zip": "B73 6RG",
+        "country": "UK"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/profile.php?id=61579255614001",
+        "instagram": "",
         "googleReview": "",
     },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.nutchathaimassage.co.uk",
+    "businessType": "HealthAndBeautyBusiness",
+    "services": "Head and Foot Reflexology, Relaxing Hot Oil Massage, Deep Tissue Massage, Full Body Massage, Thai Traditional Massage, Couples' Relaxing Hot Oil Massage, Couples' Deep Tissue Massage, Couples' Thai Traditional Massage, Hot Stone Massage",
+    "priceRange": "££",
+    "appointmentRequired": false,
+    "acceptsWalkIns": true,
+    "serviceRadius": 15000,
+
+    // Individual locations data
+    "locations": {
+        "sutton-coldfield": {
+            "name": "Nutcha Thai Massage Sutton Coldfield",
+            "email": "nutchathaimassagegroup@gmail.com",
+            "phoneForTel": "07387639263",
+            "phoneFormatted": "07387 639263",
+            "onlineBooking": "https://nutchathaimassagesutton.setmore.com",
+            "openingHours": "Mon-Sun: 10am - 8pm",
+            "googleReview": "",
+            "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.8601297!3d52.5489093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870a492ade93955%3A0xc5886c803711340e!2s440%20Chester%20Rd%20N%2C%20Sutton%20Coldfield%20B73%206RG%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
+            "address": {
+                "lineOne": "440 Chester Rd N",
+                "lineTwo": "",
+                "city": "The Royal Town of Sutton Coldfield",
+                "state": "Birmingham",
+                "zip": "B73 6RG",
+                "country": "UK",
+                "mapLink": "https://maps.app.goo.gl/BspPiLnS2nwdpyhW7"
+            },
+            "geo": {
+                "latitude": "52.5489093",
+                "longitude": "-1.8601297"
+            },
+            "socials": {
+                "facebook": "https://www.facebook.com/profile.php?id=61579255614001"
+            }
+        },
+        
+        "nuneaton": {
+            "name": "Nutcha Thai Massage Nuneaton",
+            "email": "nutchathaimassagenuneaton@gmail.com",
+            "phoneForTel": "07362433108",
+            "phoneFormatted": "07362 433108",
+            "onlineBooking": "https://nutchathaimassage-nuneaton.setmore.com",
+            "openingHours": "Mon-Sun: 10am - 8pm",
+            "googleReview": "",
+            "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.4859!3d52.5196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48774f8414181d4d%3A0xf39df1212d14c1cb!2s108%20Croft%20Rd%2C%20Nuneaton%20CV10%207DN%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
+            "address": {
+                "lineOne": "108 Croft Road",
+                "lineTwo": "",
+                "city": "Nuneaton",
+                "state": "Warwickshire",
+                "zip": "CV10 7DN",
+                "country": "UK",
+                "mapLink": "https://maps.app.goo.gl/Yr7ScYdti5L5FENo7"
+            },
+            "geo": {
+                "latitude": "52.5196",
+                "longitude": "-1.4859"
+            },
+            "socials": {
+                "facebook": "https://www.facebook.com/profile.php?id=61579255614001"
+            }
+        },
+        
+        "hinckley": {
+            "name": "Nutcha Thai Massage Hinckley",
+            "email": "nutchathaimassagegroup@gmail.com", // Placeholder - update when available
+            "phoneForTel": "07367594559",
+            "phoneFormatted": "07367 594559",
+            "onlineBooking": "https://placeholder-booking-link.com",
+            "openingHours": "Mon-Sun: 10am - 8pm",
+            "googleReview": "",
+            "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.3784!3d52.5393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48774f4d496af59b%3A0x8f2c5823dc9408f0!2s76%20Coventry%20Rd%2C%20Hinckley%20LE10%200JT%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
+            "address": {
+                "lineOne": "76 Coventry Road",
+                "lineTwo": "",
+                "city": "Hinckley",
+                "state": "Hinckley",
+                "zip": "LE10 0JT",
+                "country": "UK",
+                "mapLink": "https://maps.app.goo.gl/X74Frn79N6A5NrF86"
+            },
+            "geo": {
+                "latitude": "52.5393",
+                "longitude": "-1.3784"
+            },
+            "socials": {
+                "facebook": "https://www.facebook.com/profile.php?id=61579255614001"
+            }
+        },
+        
+        "stratford-shrieves-walk": {
+            "name": "Nutcha Thai Massage Stratford Shrieves Walk",
+            "email": "nutchathaimassagegroup@gmail.com",
+            "phoneForTel": "07309252929",
+            "phoneFormatted": "07309 252929",
+            "onlineBooking": "https://placeholder-booking-link.com",
+            "openingHours": "Mon-Sun: 10am - 8pm",
+            "googleReview": "",
+            "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.7053!3d52.192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876ef8f2db8a2d9%3A0xe74f39f3aa1f47a1!2s11-12%20Shrieves%20Walk%2C%20Stratford-upon-Avon%20CV37%206GJ%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
+            "address": {
+                "lineOne": "11-12 Shrieves Walk",
+                "lineTwo": "",
+                "city": "Stratford Upon Avon",
+                "state": "Stratford Upon Avon",
+                "zip": "CV37 6GJ",
+                "country": "UK",
+                "mapLink": "https://maps.app.goo.gl/qpeF8iZ8Tn1jo78x8"
+            },
+            "geo": {
+                "latitude": "52.192",
+                "longitude": "-1.7053"
+            },
+            "socials": {
+                "facebook": "https://www.facebook.com/profile.php?id=61579255614001"
+            }
+        },
+        
+        "stratford-henley-street": {
+            "name": "Nutcha Thai Massage Stratford Henley Street",
+            "email": "nutchathaimassagegroup@gmail.com",
+            "phoneForTel": "07496539654",
+            "phoneFormatted": "07496 539654",
+            "onlineBooking": "https://placeholder-booking-link.com",
+            "openingHours": "Mon-Sun: 10am - 8pm",
+            "googleReview": "",
+            "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.708!3d52.1937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876ef89b9c02b91%3A0xf7e6c02c5e6f5f17!2s50%20Henley%20St%2C%20Stratford-upon-Avon%20CV37%206QW%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
+            "address": {
+                "lineOne": "50 Henley Street",
+                "lineTwo": "",
+                "city": "Stratford-upon-Avon",
+                "state": "Stratford-upon-Avon",
+                "zip": "CV37 6QW",
+                "country": "UK",
+                "mapLink": "https://maps.app.goo.gl/bABiQChSt2Liv3PQA"
+            },
+            "geo": {
+                "latitude": "52.1937",
+                "longitude": "-1.708"
+            },
+            "socials": {
+                "facebook": "https://www.facebook.com/profile.php?id=61579255614001"
+            }
+        },
+        
+        "glasgow": {
+            "name": "Nutcha Thai Massage Glasgow",
+            "email": "nutchathaimassagegroup@gmail.com",
+            "phoneForTel": "07599113734",
+            "phoneFormatted": "07599 113734",
+            "onlineBooking": "https://placeholder-booking-link.com",
+            "openingHours": "Mon-Sun: 10am - 8pm",
+            "googleReview": "",
+            "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-4.2592!3d55.8606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4888442d55b5f3b9%3A0xa1f1e6db44ac7b72!2s93%20Hope%20St%2C%20Glasgow%20G2%206LD%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
+            "address": {
+                "lineOne": "Suite 287, 289, Central Chambers",
+                "lineTwo": "93 Hope St",
+                "city": "Glasgow",
+                "state": "Scotland",
+                "zip": "G2 6LD",
+                "country": "UK",
+                "mapLink": "https://maps.app.goo.gl/zpVpspufoGXe2qKz5"
+            },
+            "geo": {
+                "latitude": "55.8606",
+                "longitude": "-4.2592"
+            },
+            "socials": {
+                "facebook": "https://www.facebook.com/profile.php?id=61579255614001"
+            }
+        }
+    }
 };
+
