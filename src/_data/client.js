065071c838230a787965d817462c3de7dc86ae5b
andrewbramwell <andy@voucherstore.io>
2025-09-13 09:26:01 +0100

    setmore links

--- src/_data/client.js
+++ src/_data/client.js
@@ -35,7 +35,7 @@ module.exports = {
             "phoneFormatted": "07387 639263",
             "onlineBooking": "https://nutchathaimassagesutton.setmore.com",
             "openingHours": "Mon-Sun: 10am - 8pm",
-            "googleReview": "",
+            "googleReview": "https://g.page/r/CQ40ETeAbIjFEBM/review",
             "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.8601297!3d52.5489093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870a492ade93955%3A0xc5886c803711340e!2s440%20Chester%20Rd%20N%2C%20Sutton%20Coldfield%20B73%206RG%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
             "address": {
                 "lineOne": "440 Chester Rd N",
@@ -62,7 +62,7 @@ module.exports = {
             "phoneFormatted": "07362 433108",
             "onlineBooking": "https://nutchathaimassage-nuneaton.setmore.com",
             "openingHours": "Mon-Sun: 10am - 8pm",
-            "googleReview": "",
+            "googleReview": "https://g.page/r/CbdraQf1A-6HEBM/review",
             "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.4859!3d52.5196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48774f8414181d4d%3A0xf39df1212d14c1cb!2s108%20Croft%20Rd%2C%20Nuneaton%20CV10%207DN%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
             "address": {
                 "lineOne": "108 Croft Road",
@@ -87,9 +87,9 @@ module.exports = {
             "email": "nutchathaimassagegroup@gmail.com", // Placeholder - update when available
             "phoneForTel": "07367594559",
             "phoneFormatted": "07367 594559",
-            "onlineBooking": "https://placeholder-booking-link.com",
+            "onlineBooking": "https://nutchathaimassagehinckley.setmore.com",
             "openingHours": "Mon-Sun: 10am - 8pm",
-            "googleReview": "",
+            "googleReview": "https://g.page/r/CUS5y8xiMAudEBM/review",
             "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.3784!3d52.5393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48774f4d496af59b%3A0x8f2c5823dc9408f0!2s76%20Coventry%20Rd%2C%20Hinckley%20LE10%200JT%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
             "address": {
                 "lineOne": "76 Coventry Road",
@@ -114,9 +114,9 @@ module.exports = {
             "email": "nutchathaimassagegroup@gmail.com",
             "phoneForTel": "07309252929",
             "phoneFormatted": "07309 252929",
-            "onlineBooking": "https://placeholder-booking-link.com",
+            "onlineBooking": "https://nutchathaimassagestratfordshrieveswalk.setmore.com",
             "openingHours": "Mon-Sun: 10am - 8pm",
-            "googleReview": "",
+            "googleReview": "https://g.page/r/CdcE8RLQbWKCEBM/review",
             "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.7053!3d52.192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876ef8f2db8a2d9%3A0xe74f39f3aa1f47a1!2s11-12%20Shrieves%20Walk%2C%20Stratford-upon-Avon%20CV37%206GJ%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
             "address": {
                 "lineOne": "11-12 Shrieves Walk",
@@ -141,9 +141,9 @@ module.exports = {
             "email": "nutchathaimassagegroup@gmail.com",
             "phoneForTel": "07496539654",
             "phoneFormatted": "07496 539654",
-            "onlineBooking": "https://placeholder-booking-link.com",
-            "openingHours": "Mon-Sun: 10am - 8pm",
-            "googleReview": "",
+            "onlineBooking": "https://nutchathaimassagestratfordhenleystreet.setmore.com",
+            "openingHours": "Mon-Sun: 10am - 5pm",
+            "googleReview": "https://g.page/r/CfY6Pi1XohCOEBM/review",
             "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-1.708!3d52.1937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876ef89b9c02b91%3A0xf7e6c02c5e6f5f17!2s50%20Henley%20St%2C%20Stratford-upon-Avon%20CV37%206QW%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
             "address": {
                 "lineOne": "50 Henley Street",
@@ -168,9 +168,9 @@ module.exports = {
             "email": "nutchathaimassagegroup@gmail.com",
             "phoneForTel": "07599113734",
             "phoneFormatted": "07599 113734",
-            "onlineBooking": "https://placeholder-booking-link.com",
+            "onlineBooking": "https://nutchathaimassageglasgow.setmore.com",
             "openingHours": "Mon-Sun: 10am - 8pm",
-            "googleReview": "",
+            "googleReview": "https://g.page/r/CWlZhnrAjbUyEBM/review",
             "mapEmbed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.7!2d-4.2592!3d55.8606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4888442d55b5f3b9%3A0xa1f1e6db44ac7b72!2s93%20Hope%20St%2C%20Glasgow%20G2%206LD%2C%20UK!5e0!3m2!1sen!2suk!4v1695423897283!5m2!1sen!2suk",
             "address": {
                 "lineOne": "Suite 287, 289, Central Chambers",
